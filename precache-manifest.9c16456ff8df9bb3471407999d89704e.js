82a862bc8b27cd991af31093310f4819f2ab500a
Nevermre <invictusnevermore@gmail.com>
2019-09-15 15:27:38 -0300

    Updates

---
+++ precache-manifest.9c16456ff8df9bb3471407999d89704e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322e1506620023bbdff778d64053e14a",
+    "url": "/libras/index.html"
+  },
+  {
+    "revision": "ec8df0fcfd005f1dc260",
+    "url": "/libras/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c2ebf600820daabfeb83",
+    "url": "/libras/static/js/2.46e19150.chunk.js"
+  },
+  {
+    "revision": "ec8df0fcfd005f1dc260",
+    "url": "/libras/static/js/main.9690df63.chunk.js"
+  },
+  {
+    "revision": "a42238013dc0d2fdba59",
+    "url": "/libras/static/js/runtime~main.14a7504b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/libras/static/media/logo.5d5d9eef.svg"
+  }
+]);
